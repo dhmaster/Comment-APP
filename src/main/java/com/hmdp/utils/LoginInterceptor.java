3a42ca981329432e12443943b5cf523da73c6fff
Leo <1509861514@qq.com>
2024-03-21 14:59:52 +0800

    基于Session实现登录业务代码开发

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,34 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+
+        //1.获取session
+        HttpSession session = request.getSession();
+        //2.获取session中的用户
+        Object user = session.getAttribute("user");
+        //3.判断用户是否存在
+        if(user == null){
+            //4.不存在，拦截，返回401状态码
+            response.setStatus(401);
+            return false;
+        }
+        //5.存在，保存用户信息到Threadlocal
+        UserHolder.saveUser((UserDTO) user);
+        //6.放行
+        return true;
+    }
+
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        //移除用户
+        UserHolder.removeUser();
+    }
+}
